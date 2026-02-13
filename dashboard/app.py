b338be06e7ced0f23b774741844638ad19e5c532
Gianvito Colucci <gianvi.colucci@gmail.com>
2026-02-13 12:08:25 +0100

    dashboard/update to the more recent .run method

--- dashboard/app.py
+++ dashboard/app.py
@@ -113,4 +113,4 @@ def render_tab_content(tab):
 register_callbacks(app, data_loader)
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8050)
